0a68ebdf3e9130e4492a23f908c9017b9faf5308
Aleks Saul <aleks.saul@gmail.com>
2022-11-09 09:08:48 -0500

    origin.go: Fix Expires header

--- origin/origin.go
+++ origin/origin.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"math/rand"
 	"net/http"
+	"time"
 )
 
 func main() {
@@ -29,12 +30,17 @@ func HttpHandler(w http.ResponseWriter, r *http.Request) {
 
 	// Randomly add Expires Header or not
 	if rand.Intn(2) == 0 {
-		w.Header().Add("Expires", "foo")
+		max := 86400 // max
+		min := 6     // min 6 seconds
+		delay := rand.Intn(max-min+1) + min
+
+		timein := time.Now().UTC().Add(time.Duration(delay))
+		w.Header().Add("Expires", timein.Format(http.TimeFormat))
 	}
 
 	// Randomly add Cache-Control header
 	if rand.Intn(2) == 0 {
-		w.Header().Add("Cache-Control", fmt.Sprintf("max-age=%v", 1440))
+		w.Header().Add("Cache-Control", fmt.Sprintf("max-age=%v", 86400))
 	}
 
 	w.WriteHeader(http.StatusOK)
